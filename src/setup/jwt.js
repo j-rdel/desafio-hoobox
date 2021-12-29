46aa556e5053825a2890e2656f503188a1efcb53
Jardel Urban <jardelurban3@gmail.com>
2021-12-29 20:29:08 -0300

    Adicionando README

--- src/setup/jwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const secret = process.env.SECRET;
-
-export const sign = async(payload) => jwt.sign(payload, secret, { expiresIn: 86400 });
-export const verify = async(token) => jwt.verify(token, secret);
